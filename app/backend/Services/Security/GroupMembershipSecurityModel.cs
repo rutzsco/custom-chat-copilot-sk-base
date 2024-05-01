5e0c4c89f8dd94784dd93701b98d61257d87a5fb
Scott Rutz <scrutz@microsoft.com>
2024-05-01 12:08:10 -0500

    Update GroupMembershipSecurityModel.cs

--- app/backend/Services/Security/GroupMembershipSecurityModel.cs
+++ app/backend/Services/Security/GroupMembershipSecurityModel.cs
@@ -8,7 +8,11 @@ public static class GroupMembershipSecurityModel
 {
     public static bool HasAccess(this UserInformation userInformation, ProfileDefinition profileDefinition)
     {
-        return userInformation.Groups.Any(ug => profileDefinition.SecurityModelGroupMembership.Contains(ug));
+        if(profileDefinition.SecurityModel == "Group")
+        {
+            return userInformation.Groups.Any(ug => profileDefinition.SecurityModelGroupMembership.Contains(ug));
+        }
+        return true;
     }
 
     public static IEnumerable<ProfileDefinition> GetAuthorizedProfiles(this List<ProfileDefinition> profiles, List<string> userGroups)
