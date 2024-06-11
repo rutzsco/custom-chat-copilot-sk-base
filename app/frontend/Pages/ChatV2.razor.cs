fd99a1d457cbe7b387c5849304eb9ca1fe68c883
Scott Rutz <scrutz@microsoft.com>
2024-06-11 16:40:37 -0500

    Update ChatV2.razor.cs

--- app/frontend/Pages/ChatV2.razor.cs
+++ app/frontend/Pages/ChatV2.razor.cs
@@ -81,7 +81,7 @@ public sealed partial class ChatV2
         _files.Add(file);
 
         var buffer = new byte[file.Size];
-        await file.OpenReadStream().ReadAsync(buffer);
+        await file.OpenReadStream(2048000).ReadAsync(buffer);
         var imageContent = Convert.ToBase64String(buffer);
         _imageUrl = $"data:{file.ContentType};base64,{imageContent}";
         _imageFileName = file.Name;
