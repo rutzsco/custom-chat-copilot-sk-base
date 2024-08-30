b50c28f37288bbf3bf923f00b5569f50a879ff8b
Scott Rutz <scrutz@microsoft.com>
2024-08-30 09:34:47 -0500

    checkpoint

--- app/frontend/Components/TextInputV3.razor.cs
+++ app/frontend/Components/TextInputV3.razor.cs
@@ -1,31 +1,25 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using ClientApp.Models;
+
 namespace ClientApp.Components;
 
 public sealed partial class TextInputV3
 {
-    private string? _value;
+    private List<FileSummary> _files = new List<FileSummary>();
 
-    [Parameter] public EventCallback OnEnterKeyPressed { get; set; }
+
+    [Parameter] public EventCallback<FileSummary> OnFileUpload { get; set; }
+    [Parameter] public EventCallback<string> OnEnterKeyPressed { get; set; }
     [Parameter] public EventCallback OnResetPressed { get; set; }
 
-    [Parameter]
-    public required string? Value
-    {
-        get => _value;
-        set
-        {
-            if (_value == value)
-            {
-                return;
-            }
+    [Parameter] public EventCallback<bool> OnModelSelection { get; set; }
 
-            _value = value;
-            ValueChanged.InvokeAsync(value);
-        }
-    }
-    [Parameter] public EventCallback<string?> ValueChanged { get; set; }
+    [Parameter] public required string UserQuestion { get; set; }
+
     [Parameter] public bool Disabled { get; set; }
+    [Parameter] public bool SupportsFileUpload { get; set; }
+
     [Parameter] public string Label { get; set; } = "";
     [Parameter] public string Placeholder { get; set; } = "";
 
@@ -33,18 +27,40 @@ public sealed partial class TextInputV3
     
     private async Task OnKeyUpAsync(KeyboardEventArgs args)
     {
-        if (args is { Key: "Enter", ShiftKey: false } &&
-            OnEnterKeyPressed.HasDelegate)
+        Console.WriteLine($"OnKeyUpAsync - {UserQuestion}");
+        if (args is { Key: "Enter", ShiftKey: false } && OnEnterKeyPressed.HasDelegate)
         {
-            await OnEnterKeyPressed.InvokeAsync();
+            UserQuestion.TrimEnd('\n');
+            Console.WriteLine($"OnKeyUpAsync - {UserQuestion}");
+            await OnEnterKeyPressed.InvokeAsync(UserQuestion);
         }
     }
     private async Task OnAskClickedAsync()
     {
-        await OnEnterKeyPressed.InvokeAsync();
+        await OnEnterKeyPressed.InvokeAsync(UserQuestion);
+        UserQuestion = string.Empty;
     }
     private async Task OnClearChatAsync()
     {
+        UserQuestion = "";
+        _files.Clear();
         await OnResetPressed.InvokeAsync();
+    }
+    private async Task OnModelSelectionAsync(bool toggle)
+    {
+        await OnModelSelection.InvokeAsync(toggle);
+    }
+
+    private async Task UploadFilesAsync(IBrowserFile file)
+    {
+        Console.WriteLine("UploadFilesAsync");
+        var buffer = new byte[file.Size];
+        await file.OpenReadStream(8192000).ReadAsync(buffer);
+        var imageContent = Convert.ToBase64String(buffer);
+
+        var fileSummary = new FileSummary($"data:{file.ContentType};base64,{imageContent}", file.Name);
+        _files.Add(fileSummary);
+
+        await OnFileUpload.InvokeAsync(fileSummary);
     }
 }
---
+++ app/frontend/Components/UserInput.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace ClientApp.Components;
+
+
+public record UserInput(string Value, List<FileSummary> Files);
