8d91c15eeef08eed68239aad5c104ca17042fafe
Scott Rutz <scrutz@microsoft.com>
2024-10-18 09:26:20 -0500

    User Prompt Template support

--- app/frontend/Components/Examples.razor.cs
+++ app/frontend/Components/Examples.razor.cs
@@ -7,27 +7,11 @@ public sealed partial class Examples
     [Parameter] public required ProfileSummary Profile { get; set; }
     [Parameter, EditorRequired] public required string Message { get; set; }
     [Parameter, EditorRequired] public EventCallback<string> OnExampleClicked { get; set; }
-    [Parameter, EditorRequired] public EventCallback<string> OnPromptTemplateClicked { get; set; }
     private async Task OnClickedAsync(string exampleText)
     {
         if (OnExampleClicked.HasDelegate)
         {
             await OnExampleClicked.InvokeAsync(exampleText);
-        }
-    }
-
-    private async Task OnPromptTemplateClickedAsync(string templateName)
-    {
-        if (OnPromptTemplateClicked.HasDelegate)
-        {
-            var template = Profile.PromptTemplates.FirstOrDefault(t => t.Name == templateName);
-            var promptTemplate = Encoding.UTF8.GetString(Convert.FromBase64String(template.PromptTemplate));
-            foreach (var variable in template.Variables)
-            {
-                Console.WriteLine($"${variable.Name}:{variable.Value}");
-                promptTemplate = promptTemplate.Replace($"${variable.Name}", variable.Value);
-            }
-            await OnPromptTemplateClicked.InvokeAsync(promptTemplate);
         }
     }
 }
---
+++ app/frontend/Components/TextInputV4.razor.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Xml.Xsl;
+using ClientApp.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ClientApp.Components;
+
+public sealed partial class TextInputV4
+{
+    private List<FileSummary> _files = new List<FileSummary>();
+
+
+    [Parameter] public EventCallback<FileSummary> OnFileUpload { get; set; }
+    [Parameter] public EventCallback<string> OnEnterKeyPressed { get; set; }
+    [Parameter] public EventCallback OnResetPressed { get; set; }
+
+    [Parameter] public EventCallback<bool> OnModelSelection { get; set; }
+
+    [Parameter] public required string UserQuestion { get; set; }
+
+    [Parameter] public bool Disabled { get; set; }
+    [Parameter] public bool SupportsFileUpload { get; set; }
+
+    [Parameter] public string Label { get; set; } = "";
+    [Parameter] public string Placeholder { get; set; } = "";
+
+    [Parameter] public string ImageUrl { get; set; } = "";
+
+    [Parameter] public ProfileSummary SelectedProfileSummary { get; set; } = null;
+
+    [Parameter] public UserSelectionModel UserSelectionModel { get; set; } = null;
+
+    private async Task OnKeyUpAsync(KeyboardEventArgs args)
+    {
+        Console.WriteLine($"OnKeyUpAsync - {UserQuestion}");
+        if (args is { Key: "Enter", ShiftKey: false } && OnEnterKeyPressed.HasDelegate)
+        {
+            var question = UserQuestion;
+            UserQuestion = string.Empty;
+            question.TrimEnd('\n');
+
+            var template = SelectedProfileSummary.PromptTemplates.First();
+            var promptTemplate = Encoding.UTF8.GetString(Convert.FromBase64String(template.PromptTemplate));
+            foreach (var variable in template.Variables)
+            {
+                Console.WriteLine($"${variable.Name}:{variable.Value}");
+                promptTemplate = promptTemplate.Replace($"${variable.Name}", variable.Value);
+            }
+
+            var sb = new StringBuilder();
+            sb.Append(promptTemplate);
+            sb.AppendLine();
+            sb.Append(question);
+            Console.WriteLine($"OnKeyUpAsync - {sb.ToString()}");
+            await OnEnterKeyPressed.InvokeAsync(sb.ToString());
+        }
+    }
+    private async Task OnAskClickedAsync()
+    {
+        var question = UserQuestion;
+        UserQuestion = string.Empty;
+
+        var template = SelectedProfileSummary.PromptTemplates.First();
+        var promptTemplate = Encoding.UTF8.GetString(Convert.FromBase64String(template.PromptTemplate));
+        foreach (var variable in template.Variables)
+        {
+            Console.WriteLine($"${variable.Name}:{variable.Value}");
+            promptTemplate = promptTemplate.Replace($"${variable.Name}", variable.Value);
+        }
+
+        var sb = new StringBuilder();
+        sb.Append(promptTemplate);
+        sb.AppendLine();
+        sb.Append(question);
+        Console.WriteLine($"OnKeyUpAsync - {sb.ToString()}");
+        await OnEnterKeyPressed.InvokeAsync(sb.ToString());
+    }
+    private async Task OnClearChatAsync()
+    {
+        UserQuestion = "";
+        _files.Clear();
+        await OnResetPressed.InvokeAsync();
+    }
+    private async Task OnModelSelectionAsync(bool toggle)
+    {
+        await OnModelSelection.InvokeAsync(toggle);
+    }
+
+    private async Task UploadFilesAsync(IBrowserFile file)
+    {
+        Console.WriteLine("UploadFilesAsync");
+        var buffer = new byte[file.Size];
+        await file.OpenReadStream(8192000).ReadAsync(buffer);
+        var imageContent = Convert.ToBase64String(buffer);
+
+        var fileSummary = new FileSummary($"data:{file.ContentType};base64,{imageContent}", file.Name, file.ContentType);
+        _files.Add(fileSummary);
+
+        await OnFileUpload.InvokeAsync(fileSummary);
+    }
+}
